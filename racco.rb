f231dc98f263ea6e35237c6a33b493a33b42ec02
Daichi Sakai <daisaru11@gmail.com>
2017-11-17 17:34:07 +0900

    v0.1.0-beta.13

--- racco.rb
+++ racco.rb
@@ -5,9 +5,9 @@
 class Racco < Formula
   desc "Deployment toolbox for ECS"
   homepage ""
-  version "0.1.0-beta.12"
-  url "https://github.com/micin-jp/racco/releases/download/v0.1.0-beta.12/racco-v0.1.0-beta.12-x86_64-apple-darwin.tar.gz"
-  sha256 "ae04d19f6cd85fa6466971853a35a9850f8d1940b35e73d4339b1e01bbaab19a"
+  version "0.1.0-beta.13"
+  url "https://github.com/micin-jp/racco/releases/download/v0.1.0-beta.13/racco-v0.1.0-beta.13-x86_64-apple-darwin.tar.gz"
+  sha256 "a7b5a78129d7a9029fca8261843a3b1e3265bb1a51f92b8292352bb1d1b90b40"
 
   def install
     bin.install 'racco'
